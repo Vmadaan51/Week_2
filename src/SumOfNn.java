d131a692c4d47943b56de293f4af98127425517e
whybhav360 <vmmadaan51@gmail.com>
2024-08-24 22:42:13 +0530

    Java Programs

--- src/SumOfNn.java
+++ src/SumOfNn.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class SumOfNn {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a range");
+        System.out.println("Enter a range:");
         int n = scanner.nextInt();
-
+        int sum = 0;
+        for (int i =1; i<= n;i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
     }
 }
