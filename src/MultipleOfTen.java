8410b86e7c316002167b672485ae2ba7cd537348
whybhav360 <vmmadaan51@gmail.com>
2024-08-26 23:14:15 +0530

    files

---
+++ src/MultipleOfTen.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MultipleOfTen {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the start of the range: ");
+        int start = scanner.nextInt();
+        System.out.println("Enter the end of range: ");
+        int end = scanner.nextInt();
+        for(int i = start; i<=end;i++){
+            if (i % 10 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ src/Prime.java
@@ -0,0 +1,13 @@
+import  java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        double num = scanner.nextInt();
+        if (num % 1 == 0  && num % num == 0){
+            System.out.println("The number is prime");
+        }
+        else System.out.println("The number is not prime");
+    }
+}
