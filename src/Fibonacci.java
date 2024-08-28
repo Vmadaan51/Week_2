1c087300e04d5a66bbe5381ae3ce48766c7d5a50
whybhav360 <vmmadaan51@gmail.com>
2024-08-28 09:44:54 +0530

    .

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -5,14 +5,10 @@ public class Fibonacci {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number:");
         int num = sc.nextInt();
-
         int f = 0;  // First Fibonacci number
         int s = 1;  // Second Fibonacci number
-
         System.out.println(f);  // Print 0
         System.out.println(s);  // Print 1
-
-
         for (int i = 1; i <= num - 2; i++) {
             int fib = f + s;  // Calculate the next Fibonacci number
             System.out.println(fib);  // Print the new Fibonacci number
